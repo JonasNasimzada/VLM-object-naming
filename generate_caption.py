1e745a3b979b5eb2fbdff93a65dfd91ff2a82bb5
Jonas <jonas_nasimzada@outlook.de>
2025-08-04 06:23:36 +0200

    update script

--- generate_caption.py
+++ generate_caption.py
@@ -5,7 +5,8 @@ from os import path
 import pickle
 import sys
 from os.path import isfile, join
-
+import torch
+from transformers import BitsAndBytesConfig, pipeline, AutoModelForCausalLM
 import cv2
 from PIL import Image
 
@@ -13,7 +14,6 @@ sys.path.append(path.abspath('../Show_and_Tell'))
 
 import pandas as pd
 from huggingface_hub import hf_hub_download
-# from open_flamingo import create_model_and_transforms
 from transformers import AutoProcessor, Blip2ForConditionalGeneration
 import torch
 
@@ -22,6 +22,7 @@ MAX_TOKENS = 512  # Maximum number of tokens for caption generation
 
 
 def generate_flamingo(image):
+    from open_flamingo import create_model_and_transforms
     model, image_processor, tokenizer = create_model_and_transforms(
         clip_vision_encoder_path="ViT-L-14",
         clip_vision_encoder_pretrained="openai",
@@ -38,7 +39,7 @@ def generate_flamingo(image):
 
     tokenizer.padding_side = "left"  # For generation padding tokens should be on the left
     lang_x = tokenizer(
-        ["<image><|endofchunk|>"],  # TODO might require a different prompt
+        ["<image>\nWhat is shown in this image?<|endofchunk|>"],  # TODO might require a different prompt
         return_tensors="pt",
     )
     generated_text = model.generate(
@@ -63,55 +64,89 @@ def generate_blip2(image):
     return generated_text
 
 
-def generate_caption(image, vlm="BLIP2"):
-    if vlm == "BLIP2":
+def generate_llava(image):
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16
+    )
+    model_id = "llava-hf/llava-1.5-7b-hf"
+    pipe = pipeline("image-to-text", model=model_id, model_kwargs={"quantization_config": quantization_config},
+                    device=device)
+    prompt = "USER: <image>\nWhat is shown in this image?\nASSISTANT:"
+    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": MAX_TOKENS})
+    return outputs[0]["generated_text"]
+
+
+def generate_git(image):
+    processor = AutoProcessor.from_pretrained("microsoft/git-base-textcaps")
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-textcaps")
+    model.to(device)
+    pixel_values = pixel_values.to(device)
+
+    generated_ids = model.generate(pixel_values=pixel_values, max_length=20)
+    return processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+
+def generate_caption(image, vlm_model="BLIP2"):
+    if vlm_model == "BLIP2":
         generated_text = generate_blip2(image)
-    elif vlm == "flamingo":
+    elif vlm_model == "flamingo":
         generated_text = generate_flamingo(image)
+    elif vlm_model == "llava":
+        generated_text = generate_llava(image)
+    elif vlm_model == "git":
+        generated_text = generate_git(image)
     else:
-        raise ValueError(f"Unsupported VLM: {vlm}")
+        raise ValueError(f"Unsupported VLM: {vlm_model}")
     return generated_text
 
+
+def crop_images(dataframe):
+    for row in dataframe.itertuples():
+        image = os.path.join(args.vg_path, f"{row.vg_image_id}.jpg")
+        if not os.path.exists(image):
+            print(f"Image {image} does not exist, skipping...")
+            continue
+        full_image = cv2.imread(image, cv2.IMREAD_COLOR)
+
+        bbox_xywh = row.bbox_xywh
+        bbox_xywh = ast.literal_eval(bbox_xywh)
+        cropped_image = full_image[bbox_xywh[1]:bbox_xywh[1] + bbox_xywh[3],
+                        bbox_xywh[0]:bbox_xywh[0] + bbox_xywh[2]]
+        cv2.imwrite(f"{args.images}/{row.vg_image_id}.jpg", full_image)
+        cv2.imwrite(f"{args.images}/{row.vg_image_id}_cropped.jpg", cropped_image)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--vlm', nargs='+', required=False, choices=["BLIP2", "flamingo"], default="BLIP2")
+    parser.add_argument('--vlm', nargs='+', required=False, choices=["BLIP2", "flamingo", "llava", "git"],
+                        default="BLIP2")
     parser.add_argument('--vg_path', type=str, required=False, default="../VG_100K")
     parser.add_argument('--csv', type=str, required=False, default="manynames-en.tsv")
     parser.add_argument('--images', type=str, required=False, default="images")
+    parser.add_argument('--crop_images', type=bool, required=False, default=False)
     args = parser.parse_args()
 
-    df = pd.read_csv(args.csv, encoding="utf-8", sep="\t")
-
-    for row in df.itertuples():
-        image_path = os.path.join(args.vg_path, f"{row.vg_image_id}.jpg")
-        if not os.path.exists(image_path):
-            print(f"Image {image_path} does not exist, skipping...")
-            continue
-        original_image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-
-        bbox_xywh = row.bbox_xywh
-        bbox_xywh = ast.literal_eval(bbox_xywh)
-        cropped_image = original_image[bbox_xywh[1]:bbox_xywh[1] + bbox_xywh[3],
-                        bbox_xywh[0]:bbox_xywh[0] + bbox_xywh[2]]
-        cv2.imwrite(f"{args.images}/{row.vg_image_id}.jpg", cropped_image)
-        cv2.imwrite(f"{args.images}/{row.vg_image_id}_cropped.jpg", cropped_image)
+    if args.crop_images:
+        df = pd.read_csv("manynames-en.tsv", encoding="utf-8", sep="\t")
+        crop_images(df)
 
     images = [f for f in os.listdir(args.images) if isfile(join(args.images, f))]
     for vlm in args.vlm:
         caption_dict = {}
         i = 0
-        for image in images:
-            original_image = cv2.imread(os.path.join(args.images, image), cv2.IMREAD_COLOR)
+        for image_path in images:
+            original_image = cv2.imread(os.path.join(args.images, image_path), cv2.IMREAD_COLOR)
 
-            caption_image = generate_caption(original_image, vlm=vlm)
+            caption_image = generate_caption(original_image, vlm_model=vlm)
 
-            caption_dict[row.vg_object_id] = caption_image
+            caption_dict[image_path] = caption_image
             if i % 1000 == 1:
                 with open(f'captions/{vlm}.pkl', 'wb') as f:
                     pickle.dump(caption_dict, f)
+                print("image number:", i, "image:", image_path)
 
             i += 1
 
